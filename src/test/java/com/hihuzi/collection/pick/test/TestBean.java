ba248aeaf313d7be129d6386fd536b8da4e2d2a2
hi@hu.zi <hi@hu.zi>
2018-10-18 15:00:29 +0800

    无极递归提取父类属性参数 优化结构 提升效能

---
+++ src/test/java/com/hihuzi/collection/pick/test/TestBean.java
@@ -0,0 +1,25 @@
+package com.hihuzi.collection.pick.test;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.util.Date;
+
+/**
+ * tips 测试bean
+ *
+ * @author: hihuzi  2018/6/27 6:39
+ */
+@Data
+@EqualsAndHashCode(callSuper=false)
+public class TestBean extends Base {
+
+    private String id;
+
+    private String name;
+
+    private String email;
+
+    private String address;
+    private Date date = new Date();
+}
